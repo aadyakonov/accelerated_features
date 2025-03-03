e72653a683ba71a0b092f3299f38c5f03d4997f0
walkinggat <alexander.a.dyakonov@gmail.com>
2025-03-03 19:18:57 +0800

    added local pth support

--- hubconf.py
+++ hubconf.py
@@ -2,14 +2,17 @@ dependencies = ['torch']
 from modules.xfeat import XFeat as _XFeat
 import torch
 
-def XFeat(pretrained=True, top_k=4096, detection_threshold=0.05):
+def XFeat(pretrained=True, top_k=4096, detection_threshold=0.05, custom=False, weights_path=None):
     """
     XFeat model
     pretrained (bool): kwargs, load pretrained weights into the model
     """
     weights = None
     if pretrained:
-        weights = torch.hub.load_state_dict_from_url("https://github.com/verlab/accelerated_features/raw/main/weights/xfeat.pt")
+        if not custom:
+            weights = torch.hub.load_state_dict_from_url("https://github.com/verlab/accelerated_features/raw/main/weights/xfeat.pt")
+        else:
+            weights = torch.load(weights_path)
     
     model = _XFeat(weights, top_k=top_k, detection_threshold=detection_threshold)
     return model
