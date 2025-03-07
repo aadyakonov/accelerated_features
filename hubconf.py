af36ca70418ce0d1722b788085c49a981069c132
diixo <anatoliy.podoba@dxc.com>
2025-03-07 06:29:25 +0200

    Fixed device pre-set

--- hubconf.py
+++ hubconf.py
@@ -7,12 +7,20 @@ def XFeat(pretrained=True, top_k=4096, detection_threshold=0.05, custom=False, w
     XFeat model
     pretrained (bool): kwargs, load pretrained weights into the model
     """
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     weights = None
     if pretrained:
         if not custom:
-            weights = torch.hub.load_state_dict_from_url("https://github.com/verlab/accelerated_features/raw/main/weights/xfeat.pt")
+            weights = torch.hub.load_state_dict_from_url(
+                "https://github.com/verlab/accelerated_features/raw/main/weights/xfeat.pt",
+                map_location=device
+                )
         else:
-            weights = torch.load(weights_path)
+            weights = torch.load(
+                weights_path,
+                map_location=device
+                )
     
     model = _XFeat(weights, top_k=top_k, detection_threshold=detection_threshold)
     return model
